110e470d250c58d21df253bd72c610dd9d2f2e27
yufieko <yufieko@gmail.com>
2015-06-15 14:00:51 +0700

    update config

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 // facebook app
-define('APP_ID','841753505904139');
-define('APP_SECRET', 'c33c20dd54a7bdc0c5dd0fa1a573b6fa');
+define('APP_ID','APP ID');
+define('APP_SECRET', 'APP SECRET');
 $scope = 'publish_actions, public_profile, email, user_photos, user_about_me, user_birthday, user_hometown, user_location, user_relationships';
 define('APP_SCOPE', $scope);
 // mendefinisikan base_url dinamis
